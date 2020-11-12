34df93aead4a197b95478e2b2face587276a1f9e
andrey <2013shandy@gmail.com>
2020-11-13 01:19:05 +0200

    add border color text

--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h4> ПРИВЕТ ЭТО ПЕРВЫЕ СЛОВА!!!</h4>")
+    return render(request, 'main/index.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
